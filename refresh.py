41cbc5cf87c928bb3cf8c02b39892c0d04fb1f8e
心之所向 <2583334799@qq.com>
2017-07-01 23:01:30 +0800

    最初版本

---
+++ refresh.py
@@ -0,0 +1,25 @@
+import random
+from socket import *
+ResponseHeader=b"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nAccept-Ranges: bytes\r\n\r\n"
+HOST='localhost'
+PORT=7921
+ADDR=(HOST,PORT)
+s=socket(AF_INET,SOCK_STREAM)
+s.bind(ADDR)
+s.listen(5)
+while True:
+    c,addr=s.accept()
+    buff=c.recv(1024)
+    if buff.find(b'GET')>=0 and buff.find(b'HTTP')>0 and buff.find(b'date.txt')<0:
+        c.send(ResponseHeader)
+        f=open('index.html','r')
+        while True:
+            line=f.readline()
+            c.send(bytes(line,encoding='utf-8'))
+            if not line:
+                break
+        f.close()
+    if buff.find(b'GET')>=0 and buff.find(b'date.txt')>0:
+        c.send(ResponseHeader)     
+        c.send(bytes(str(random.uniform(0,50)),encoding='utf-8'))           
+    c.close()
